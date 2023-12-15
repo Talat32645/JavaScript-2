5e5e19c8bd99fc6e530c7db7ecfb11ee74f1aebd
Talat32645 <151937250+Talat32645@users.noreply.github.com>
2023-12-15 14:31:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var age = +prompt ('Введите ваш возраст');
+
+
+if (age < 18 ) {
+    alert('Вы слишком юны')
+}else if(age >= 18 & age < 60){
+    alert('Работай')
+}else if ( age > 60){
+    alert('оТДЫХАЙ')
+}
+
+var time = +prompt (' Введите время ')
+
+
+
